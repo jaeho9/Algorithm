ea9c7a67fd54edf3a4a28012b8e8ff1b1a0d66d2
jaeho9 <68535086+jaeho9@users.noreply.github.com>
2024-03-20 14:39:42 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.65 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,14 @@
+function solution(A,B){
+    var answer = 0;
+    
+    A.sort(function(a, b)  {return a - b;});
+    B.sort(function(a, b)  {return b - a;});
+    
+    
+     for(let i=0; i<A.length; i++){
+         answer+=(A[i]*B[i]);     
+     }
+    
+
+    return answer;
+}
