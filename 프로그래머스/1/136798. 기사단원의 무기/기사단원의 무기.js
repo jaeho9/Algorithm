6a555541b4c46d482098696a272d7ad2a7de5cde
LeeJaeHo <68535086+jaeho9@users.noreply.github.com>
2025-02-21 03:09:19 +0900

    [level 1] Title: 기사단원의 무기, Time: 105.54 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,26 +1,19 @@
 function solution(number, limit, power) {
-    var answer = 0;
-    let tmp_arr  = []; // 약수의 개수를 넣을 배열
-    
-    for(let i=1; i<=number; i++){
+    let answer = 0;
+    let divisors = [];
+
+    for (let i = 1; i <= number; i++) {
         let cnt = 0;
-        let idx = 1;
-        for(let j = 0; j*j<=i; j++){
-            if(i % j === 0){
-                cnt += (j*j === i) ? 1 : 2;
+        for (let j = 1; j * j <= i; j++) {
+            if (i % j === 0) {
+                cnt += (j * j === i) ? 1 : 2;
             }
         }
-        tmp_arr.push(cnt);
+        divisors.push(cnt);
     }
-    
-    console.log(tmp_arr);
-    
-    for(let i=0; i<tmp_arr.length; i++){
-        if(tmp_arr[i] <= limit){
-            answer+=tmp_arr[i];
-        }else{
-            answer+=power;
-        }
+
+    for (const cnt of divisors) {
+        answer += (cnt <= limit) ? cnt : power;
     }
 
     return answer;
